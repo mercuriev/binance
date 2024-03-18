313383a1e783d5f9bc884f6efb840635f0973f25
Max <chepuhovia@gmail.com>
2024-03-18 22:11:16 +0700

    verbose message for InsuficcientBalance

--- src/Exception/InsuficcientBalance.php
+++ src/Exception/InsuficcientBalance.php
@@ -1,6 +1,22 @@
 <?php
 namespace Binance\Exception;
 
+use Laminas\Http\Request;
+use Laminas\Http\Response;
+
 class InsuficcientBalance extends BinanceException
 {
+    public function __construct(Request $req, Response|array $res)
+    {
+        parent::__construct($req, $res);
+
+        $o = $req->getPost();
+        $this->message = sprintf(
+            'Account has insufficient balance to %s %s %.'.($o['quantity']?5:2).'f for %.2f',
+            $o['side'],
+            $o['symbol'],
+            $o['quantity'] ?? $o['quoteOrderQty'],
+            $o['price']
+        );
+    }
 }
