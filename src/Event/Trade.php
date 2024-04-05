36bf9bdd699e4af3cc59ef8f14485f4e37ad5020
Max <chepuhovia@gmail.com>
2024-04-05 19:21:41 +0700

    Reformat code in Trade event class
    The commit introduces a more readable format to the code within the 'Trade' event class. Parameters have been realigned to improve clarity and maintain code consistency throughout the project. No functional changes were made in the code.

--- src/Event/Trade.php
+++ src/Event/Trade.php
@@ -17,14 +17,14 @@ class Trade extends Event
     /** @noinspection PhpMissingParentConstructorInspection */
     public function __construct(array $payload)
     {
-        $this->time = new \DateTime("@".intval($payload['E']/1000));
-        $this->symbol = $payload['s'];
-        $this->id = $payload['t'];
-        $this->price = floatval($payload['p']);
-        $this->quantity = floatval($payload['q']);
-        $this->buyerOrderId = $payload['b'];
-        $this->sellerOrderId = $payload['a'];
-        $this->tradeTime = new \DateTime("@".intval($payload['T']/1000));
-        $this->buyerIsMaker = $payload['m'];
+        $this->time           = new \DateTime("@".intval($payload['E']/1000));
+        $this->symbol         = $payload['s'];
+        $this->id             = $payload['t'];
+        $this->price          = floatval($payload['p']);
+        $this->quantity       = floatval($payload['q']);
+        $this->buyerOrderId   = $payload['b'];
+        $this->sellerOrderId  = $payload['a'];
+        $this->tradeTime      = new \DateTime("@".intval($payload['T']/1000));
+        $this->buyerIsMaker   = $payload['m'];
     }
 }
