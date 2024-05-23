33f96aa3cae1dcd06dd9401d20cb1300037d1e9d
Max <chepuhovia@gmail.com>
2024-05-23 08:21:58 +0800

    Add properties to LimitMakerOrder class
    Several new properties have been added to the LimitMakerOrder class. These include orderId, orderListId, clientOrderId, quantity, transactTime, origQty, executedQty, cummulativeQuoteQty, quoteOrderQty and newOrderRespType.

--- src/Order/LimitMakerOrder.php
+++ src/Order/LimitMakerOrder.php
@@ -2,6 +2,19 @@
 
 namespace Binance\Order;
 
+/**
+ * @property int $orderId
+ * @property int $orderListId
+ * @property string $newClientOrderId
+ * @property string $clientOrderId
+ * @property float $quantity
+ * @property int $transactTime
+ * @property int $origQty
+ * @property int $executedQty
+ * @property int $cummulativeQuoteQty
+ * @property float $quoteOrderQty
+ * @property string $newOrderRespType
+ */
 class LimitMakerOrder extends AbstractOrder
 {
     public float $price;
