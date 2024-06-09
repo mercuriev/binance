a738f19bfaa061ef29e5999992463113591763d5
Max Mercuriev <max@yogi.pw>
2024-06-10 02:20:44 +0800

    properties hints for Account

--- src/Account/Account.php
+++ src/Account/Account.php
@@ -4,6 +4,24 @@ namespace Binance\Account;
 
 use Binance\AbstractPayload;
 
+/**
+ * @property int makerCommission
+ * @property int takerCommission
+ * @property int buyerCommission
+ * @property int sellerCommission
+ * @property array commissionRates
+ * @property bool canTrade
+ * @property bool canWithdraw
+ * @property bool canDeposit
+ * @property bool brokered
+ * @property bool requireSelfTradePrevention
+ * @property bool preventSor
+ * @property int updateTime
+ * @property string accountType
+ * @property array balances
+ * @property array permissions
+ * @property int uid
+ */
 class Account extends AbstractPayload
 {
 }
